8ea37948b29c705b79db390029d75880b75e873c
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-05-18 08:18:08 -0400

    Using server-side-props

--- pages/index.js
+++ pages/index.js
@@ -23,14 +23,24 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//     // using revalidate will make the server side re-render a new static
+//     // if the request is more than 10 seconds old from the previous request.
+//     revalidate: 10,
+//   };
+// }
+
+// This will make the server-side re-render the page on every request
+// and then send a static page back to the client
+export async function getServerSideProps(context) {
+  const req = context.req;
+  const res = context.res;
   return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-    // using revalidate will make the server side re-render a new static
-    // if the request is more than 10 seconds old from the previous request.
-    revalidate: 10,
+    props: DUMMY_MEETUPS,
   };
 }
 
