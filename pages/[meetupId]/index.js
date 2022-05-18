81050707b0519f2614e99c12f1ebf5cbbff57c0a
Aly Badawy <AlyBadawy@users.noreply.github.com>
2022-05-18 07:48:13 -0400

    Meetup pages
    Signed-off-by: Aly Badawy <AlyBadawy@users.noreply.github.com>

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetails from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetailsPage() {
+  return (
+    <MeetupDetails
+      title="A third meetup"
+      address="some Address"
+      image="https://images.unsplash.com/1/bag-and-hands.jpg?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1466&q=80"
+      description="Description for the third meetup"
+    />
+  );
+}
